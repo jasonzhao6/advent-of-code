d1361f2cca18ddb148d850ea83df45c800f91f1b
Jason Zhao <jasonzhao6@gmail.com>
2019-01-09 20:45:26 -0800

    Add boilerplate for finding most probable coordinate

--- 2018/day23/try.rb
+++ 2018/day23/try.rb
@@ -7,13 +7,6 @@ class Try
     @filename = filename
   end
 
-  def bots
-    return @bots ||= File.open(filename).map do |line|
-      x, y, z, r = line.scan(/-?\d+/).map(&:to_i)
-      { x: x, y: y, z: z, r: r }
-    end
-  end
-
   def strongest
     return @strongest ||= bots.each_with_object({ r: 0 }) do |bot, strongest|
       strongest.merge!(bot) if bot[:r] > strongest[:r]
@@ -33,11 +26,52 @@ class Try
       dt <= origin[:r]
     end
   end
+
+  def most_probable
+    hash = {}
+
+    (min(:x)..max(:x)).each do |x|
+      (min(:y)..max(:y)).each do |y|
+        (min(:z)..max(:z)).each do |z|
+          hash[[x, y, z].join(',')] = 0
+        end
+      end
+    end
+
+    hash
+  end
+
+  private
+
+  def bots
+    return @bots ||= File.open(filename).map do |line|
+      x, y, z, r = line.scan(/-?\d+/).map(&:to_i)
+      { x: x, y: y, z: z, r: r }
+    end
+  end
+
+  def min(symbol)
+    @min ||= {}
+    return @min[symbol] if @min[symbol]
+
+    @min[symbol] = bots.each_with_object({ symbol => 0 }) do |bot, obj|
+      obj[symbol] = bot[symbol] if bot[symbol] < obj[symbol]
+    end[symbol]
+  end
+
+  def max(symbol)
+    @max ||= {}
+    return @max[symbol] if @max[symbol]
+
+    @max[symbol] = bots.each_with_object({ symbol => 0 }) do |bot, obj|
+      obj[symbol] = bot[symbol] if bot[symbol] > obj[symbol]
+    end[symbol]
+  end
 end
 
 # Part 1
 # try = Try.new('input_a.txt')
 # p try.in_range(try.strongest).size
 
-try = Try.new('input.txt')
-p try.in_range(try.strongest).size
+try = Try.new('input_a.txt')
+p try.most_probable
