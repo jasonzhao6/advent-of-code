5736c4d78f7ed1959166a9130d1e42820dc2b03e
Jason Zhao <jasonzhao6@gmail.com>
2019-01-09 21:35:17 -0800

    Add brute force algorithm for finding most probable coordinate

--- 2018/day23/try.rb
+++ 2018/day23/try.rb
@@ -14,29 +14,40 @@ class Try
   end
 
   def in_range(origin)
-    bots.select do |bot|
-      distance(origin, bot) <= origin[:r]
-    end
+    bots.select { |bot| distance(origin, bot) <= origin[:r] }
   end
 
   def most_probable
-    hash = {}
+    hash = Hash.new(0)
 
     (min(:x)..max(:x)).each do |x|
       (min(:y)..max(:y)).each do |y|
         (min(:z)..max(:z)).each do |z|
-          hash[[x, y, z].join(',')] = 0
+          origin = { x: x, y: y, z: z }
+          bots.each do |bot|
+            hash[origin] += 1 if distance(origin, bot) <= bot[:r]
+          end
         end
       end
     end
 
-    hash
+    hash.each_with_object(score: 0, distance: 0) do |(origin, score), winner|
+      higher_score = score > winner[:score]
+      same_score = score == winner[:score]
+
+      distance = origin[:x] + origin[:y] + origin[:z]
+      closer = distance < winner[:distance]
+
+      if higher_score || (same_score && closer)
+        winner.merge!(origin).merge!(distance: distance, score: score)
+      end
+    end
   end
 
   private
 
   def bots
-    return @bots ||= File.open(filename).map do |line|
+    @bots ||= File.open(filename).map do |line|
       x, y, z, r = line.scan(/-?\d+/).map(&:to_i)
       { x: x, y: y, z: z, r: r }
     end
@@ -72,5 +83,5 @@ end
 # try = Try.new('input_a.txt')
 # p try.in_range(try.strongest).size
 
-try = Try.new('input_a.txt')
+try = Try.new('input_b.txt')
 p try.most_probable
