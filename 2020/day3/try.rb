c49f2630a9026d648164a2eee8b54a017adff767
jasonzhao6 <jasonzhao6@gmail.com>
2020-12-02 21:56:01 -0800

    Day 3

---
+++ 2020/day3/try.rb
@@ -0,0 +1,46 @@
+map = []
+
+File.open('input.txt').each do |line|
+  map << line.chomp.chars * 100
+end
+
+#
+# Part 1
+#
+
+cursor = 0
+count = 0
+
+map.each do |line|
+  count += 1 if line[cursor] == '#'
+  cursor += 3
+end
+
+#
+# Part 2
+#
+
+counts = []
+
+slope = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+]
+
+slope.each do |x, y|
+  cursor = 0
+  count = 0
+
+  map.each.with_index do |line, index|
+    next if index.odd? && y == 2
+    count += 1 if line[cursor] == '#'
+    cursor += x
+  end
+
+  counts << count
+end
+
+p counts.reduce(:*)
