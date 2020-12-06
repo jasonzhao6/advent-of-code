62c6d70491f444d9e7e123752e7963c09c7a0803
jasonzhao6 <jasonzhao6@gmail.com>
2020-12-05 22:38:37 -0800

    Instead of tallying answers, intersect them
    Took this insight from Angel's solution. It's much cleaner.

--- 2020/day6/try.rb
+++ 2020/day6/try.rb
@@ -18,16 +18,8 @@ p counts.sum
 file = File.open('input.txt').read
 groups = file.split("\n\n")
 counts = groups.map do |group|
-  hash = Hash.new(0)
-
   answers = group.split("\n")
-  answers.each do |answer|
-    answer.chars.each do |char|
-      hash[char] += 1
-    end
-  end
-
-  hash.values.count { |value| value == answers.count }
+  answers.map { |answer| answer.chars }.reduce(:&).count
 end
 
 p counts.sum
