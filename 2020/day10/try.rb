f167ed518274f70b9da13768e1c6f74107ec0fb4
jasonzhao6 <jasonzhao6@gmail.com>
2020-12-09 22:13:50 -0800

    Day 10

---
+++ 2020/day10/try.rb
@@ -0,0 +1,69 @@
+require 'set'
+
+#
+# Part 1
+#
+
+# class Program
+#   def initialize(input)
+#     @input = input.map(&:to_i)
+#
+#     @ones = 0
+#     @threes = 0
+#   end
+#
+#   def run
+#     nums = @input.map(&:to_i).sort
+#     nums.each.with_index do |num, i|
+#       if i == 0
+#         prev = 0
+#       else
+#         prev = nums[i - 1]
+#       end
+#
+#       if num - prev == 1
+#         @ones += 1
+#       elsif num - prev == 3
+#         @threes += 1
+#       else
+#         raise '!'
+#       end
+#     end
+#
+#     p [@ones, @threes, @ones * (@threes + 1)]
+#   end
+# end
+#
+# input = File.open('input.txt').read.split("\n")
+# program = Program.new(input)
+# program.run
+
+#
+# Part 2
+#
+
+class Program
+  def initialize(input)
+    @input = input.map(&:to_i)
+
+    @nums = @input.map(&:to_i).sort.unshift(0).reverse
+    @cache = {}
+  end
+
+  def recurse(current)
+    return 1 if current == 0
+    return @cache[current] if @cache[current]
+
+    prevs = @nums.select { |num| num >= current - 3 && num < current }
+    @cache[current] = prevs.map { |prev| recurse(prev) }.sum
+    return @cache[current]
+  end
+
+  def run
+    p recurse(@nums.first)
+  end
+end
+
+input = File.open('input.txt').read.split("\n")
+program = Program.new(input)
+program.run
