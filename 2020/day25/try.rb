f8d23b7eca75e345caf1839acdcadcf8c10c548c
jasonzhao6 <jasonzhao6@gmail.com>
2020-12-26 14:07:04 -0800

    Add notes on share secret

--- 2020/day25/try.rb
+++ 2020/day25/try.rb
@@ -1,3 +1,13 @@
+# Alice and Bob establishing shared secret:
+#
+# shared secret
+#  = (subject ** a_secret) ** b_secret   ==>   (subject ** a_secret) = a_public (mod n)
+#  = (subject ** b_secret) ** a_secret   ==>   (subject ** b_secret) = b_public (mod n)
+#
+# In this problem, we are given the subject and both public keys.
+# From them, we can calculate either one of the private keys.
+# Once we have a private key, we can calculate the shared secret.
+
 a = 5764801  # Sample
 b = 17807724 # Sample
 
