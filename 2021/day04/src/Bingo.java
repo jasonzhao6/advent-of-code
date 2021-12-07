aee130fd3a71f81890fdf82e38a1e1317d51c0fb
Jason Zhao <jasonzhao6@gmail.com>
2021-12-06 17:39:33 -0800

    Day 4

---
+++ 2021/day04/src/Bingo.java
@@ -0,0 +1,61 @@
+public class Bingo {
+    public final static int SIZE = 5;
+    private final int[][] board;
+    private final boolean[][] marks;
+
+    public Bingo(int[][] board) {
+        this.board = board;
+        this.marks = new boolean[SIZE][SIZE];
+    }
+
+    public void mark(int value) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == value) {
+                    this.marks[i][j] = true;
+                }
+            }
+        }
+    }
+
+    public int unmarkedSum() {
+        int sum = 0;
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (marks[i][j] == false) {
+                    sum += this.board[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    public boolean hasWon() {
+        for (int i = 0; i < SIZE; i++) {
+            boolean hasRowWon = true;
+            boolean hasColWon = true;
+
+            for (int j = 0; j < SIZE; j++) {
+                if (!marks[i][j]) {
+                    hasRowWon = false;
+                    break;
+                }
+            }
+
+            for (int j = 0; j < SIZE; j++) {
+                if (!marks[j][i]) {
+                    hasColWon = false;
+                    break;
+                }
+            }
+
+            if (hasRowWon || hasColWon) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ 2021/day04/src/Try.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Try {
+    static final String filename = "input.txt";
+
+    static void p(Object... objects) {
+        boolean isFirst = false;
+
+        for (Object object : objects) {
+            if (!isFirst) {
+                isFirst = true;
+            } else {
+                System.out.print(", ");
+            }
+
+            if (object.getClass().getComponentType() == int.class) {
+                System.out.print(Arrays.toString((int[]) object));
+            } else if (object.getClass().isArray()) {
+                System.out.print(Arrays.toString((Object[]) object));
+            } else {
+                System.out.print(object);
+            }
+        }
+
+        System.out.println();
+    }
+
+    static List<String> readLines() {
+        try {
+            return Files.readAllLines(Paths.get(Try.filename));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> lines = readLines();
+
+        // Parse numbers drawn, which is on line 1
+        List<Integer> numbers = Arrays
+                .asList(lines.get(0).split(","))
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        // Parse bingo boards, which starts on line 3
+        // There is an empty line between each board, hence the SIZE + 1
+        List<Bingo> boards = new ArrayList<>();
+        for (int i = 2; i < lines.size(); i += Bingo.SIZE + 1) {
+            int[][] board = new int[Bingo.SIZE][];
+
+            for (int j = 0; j < Bingo.SIZE; j++) {
+                board[j] = Arrays
+                        .stream(lines.get(i + j).split(" "))
+                        .filter(s -> !s.isEmpty())
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+            }
+
+            boards.add(new Bingo(board));
+        }
+
+        part1(numbers, boards);
+        part2(numbers, boards);
+    }
+
+    public static void part1(List<Integer> numbers, List<Bingo> boards) {
+        // Feed each number to each board, and check for winner each time
+        for (int number : numbers) {
+            for (Bingo board : boards) {
+                board.mark(number);
+
+                if (board.hasWon()) {
+                    p(number, board.unmarkedSum(), number * board.unmarkedSum());
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void part2(List<Integer> numbers, List<Bingo> boards) {
+        Set<Bingo> winners = new HashSet<>();
+
+        // Feed each number to each board, and find the last winner
+        for (int number : numbers) {
+            for (Bingo board : boards) {
+                board.mark(number);
+
+                if (board.hasWon()) {
+                    winners.add(board);
+                }
+
+                if (winners.size() == boards.size()) {
+                    p(winners.size(), number, board.unmarkedSum(), number * board.unmarkedSum());
+                    System.exit(0);
+                }
+            }
+        }
+    }
+}
